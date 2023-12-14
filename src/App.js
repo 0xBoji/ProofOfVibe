46ab18ccb77f479efc3f0204db1d70969efd3adb
Alexandro De Aries <126134422+pichtranst123@users.noreply.github.com>
2023-12-14 16:43:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,221 +1,183 @@
-const TestnetDomains = {
-  "test.near.social": true,
-  "127.0.0.1": true,
-};
+import React, { useCallback, useEffect, useState } from "react";
+import "error-polyfill";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@near-wallet-selector/modal-ui/styles.css";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import "react-bootstrap-typeahead/css/Typeahead.bs5.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "App.scss";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import EditorPage from "./pages/EditorPage";
+import ViewPage from "./pages/ViewPage";
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupSender } from "@near-wallet-selector/sender";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import { setupNeth } from "@near-wallet-selector/neth";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import EmbedPage from "./pages/EmbedPage";
+import { sanitizeUrl } from "@braintree/sanitize-url";
+import {
+  useAccount,
+  useInitNear,
+  useNear,
+  utils,
+  EthersProviderContext,
+} from "near-social-vm";
+import Big from "big.js";
+import { NavigationWrapper } from "./components/navigation/NavigationWrapper";
+import { NetworkId, Widgets } from "./data/widgets";
+import { useEthersProviderContext } from "./data/web3";
+import { Camera } from "./components/custom/Camera";
 
-export const NetworkId =
-  window.location.hostname in TestnetDomains ? "testnet" : "mainnet";
-const TestnetWidgets = {
-  image: "eugenethedream/widget/Image",
-  default: "eugenethedream/widget/Welcome",
-  viewSource: "eugenethedream/widget/WidgetSource",
-  widgetMetadataEditor: "eugenethedream/widget/WidgetMetadataEditor",
-  widgetMetadata: "eugenethedream/widget/WidgetMetadata",
-  profileImage: "eugenethedream/widget/ProfileImage",
-  profilePage: "eugenethedream/widget/Profile",
-  profileName: "eugenethedream/widget/ProfileName",
-  notificationButton: "eugenethedream/widget/NotificationButton",
-};
+export const refreshAllowanceObj = {};
+const documentationHref = "https://docs.ProofOfVibes.com";
 
-const MainnetWidgets = {
-  image: "mob.near/widget/Image",
-  default: "proofofvibes.near/widget/Vibes.DAO.main",
-  viewSource: "mob.near/widget/WidgetSource",
-  widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
-  widgetMetadata: "mob.near/widget/WidgetMetadata",
-  profileImage: "mob.near/widget/ProfileImage",
-  notificationButton: "mob.near/widget/NotificationButton",
-  profilePage: "near/widget/ProfilePage",
-  profileName: "patrick.near/widget/ProfileName",
-  editorComponentSearch: "mob.near/widget/Editor.ComponentSearch",
-  profileInlineBlock: "mob.near/widget/Profile.InlineBlock",
-  // viewHistory: "bozon.near/widget/WidgetHistory",
-  viewHistory: "near/widget/ComponentHistory",
-};
+function App(props) {
+  const [connected, setConnected] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
+  const [signedAccountId, setSignedAccountId] = useState(null);
+  const [availableStorage, setAvailableStorage] = useState(null);
+  const [walletModal, setWalletModal] = useState(null);
+  const [widgetSrc, setWidgetSrc] = useState(null);
 
-export const Widgets =
-  NetworkId === "testnet" ? TestnetWidgets : MainnetWidgets;
-  import React, { useCallback, useEffect, useState } from "react";
-  import "error-polyfill";
-  import "bootstrap-icons/font/bootstrap-icons.css";
-  import "@near-wallet-selector/modal-ui/styles.css";
-  import "react-bootstrap-typeahead/css/Typeahead.css";
-  import "react-bootstrap-typeahead/css/Typeahead.bs5.css";
-  import "bootstrap/dist/js/bootstrap.bundle";
-  import "App.scss";
-  import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-  import EditorPage from "./pages/EditorPage";
-  import ViewPage from "./pages/ViewPage";
-  import { setupWalletSelector } from "@near-wallet-selector/core";
-  import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-  import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-  import { setupSender } from "@near-wallet-selector/sender";
-  import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-  import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-  import { setupNeth } from "@near-wallet-selector/neth";
-  import { setupModal } from "@near-wallet-selector/modal-ui";
-  import EmbedPage from "./pages/EmbedPage";
-  import { sanitizeUrl } from "@braintree/sanitize-url";
-  import {
-    useAccount,
-    useInitNear,
-    useNear,
-    utils,
-    EthersProviderContext,
-  } from "near-social-vm";
-  import Big from "big.js";
-  import { NavigationWrapper } from "./components/navigation/NavigationWrapper";
-  import { NetworkId, Widgets } from "./data/widgets";
-  import { useEthersProviderContext } from "./data/web3";
-  import { Camera } from "./components/custom/Camera";
-  
-  export const refreshAllowanceObj = {};
-  const documentationHref = "https://docs.ProofOfVibes.com";
-  
-  function App(props) {
-    const [connected, setConnected] = useState(false);
-    const [signedIn, setSignedIn] = useState(false);
-    const [signedAccountId, setSignedAccountId] = useState(null);
-    const [availableStorage, setAvailableStorage] = useState(null);
-    const [walletModal, setWalletModal] = useState(null);
-    const [widgetSrc, setWidgetSrc] = useState(null);
-  
-    const ethersProviderContext = useEthersProviderContext();
-  
-    const { initNear } = useInitNear();
-    const near = useNear();
-    const account = useAccount();
-    const accountId = account.accountId;
-  
-    useEffect(() => {
-      initNear &&
-        initNear({
-          networkId: NetworkId,
-          selector: setupWalletSelector({
-            network: NetworkId,
-            modules: [
-              setupNearWallet(),
-              setupMyNearWallet(),
-              setupSender(),
-              setupHereWallet(),
-              setupMeteorWallet(),
-              setupNeth({
-                gas: "300000000000000",
-                bundle: false,
-              }),
-            ],
-          }),
-          customElements: {
-            Link: (props) => {
-              if (!props.to && props.href) {
-                props.to = props.href;
-                delete props.href;
-              }
-              if (props.to) {
-                props.to = sanitizeUrl(props.to);
-              }
-              return <Link {...props} />;
-            },
-            Camera: (props) => {
-              return <Camera {...props} />
+  const ethersProviderContext = useEthersProviderContext();
+
+  const { initNear } = useInitNear();
+  const near = useNear();
+  const account = useAccount();
+  const accountId = account.accountId;
+
+  useEffect(() => {
+    initNear &&
+      initNear({
+        networkId: NetworkId,
+        selector: setupWalletSelector({
+          network: NetworkId,
+          modules: [
+            setupNearWallet(),
+            setupMyNearWallet(),
+            setupSender(),
+            setupHereWallet(),
+            setupMeteorWallet(),
+            setupNeth({
+              gas: "300000000000000",
+              bundle: false,
+            }),
+          ],
+        }),
+        customElements: {
+          Link: (props) => {
+            if (!props.to && props.href) {
+              props.to = props.href;
+              delete props.href;
             }
+            if (props.to) {
+              props.to = sanitizeUrl(props.to);
+            }
+            return <Link {...props} />;
           },
-        });
-    }, [initNear]);
-  
-    useEffect(() => {
-      if (!near) {
-        return;
-      }
-      near.selector.then((selector) => {
-        setWalletModal(
-          setupModal(selector, { contractId: near.config.contractName })
-        );
+          Camera: (props) => {
+            return <Camera {...props} />
+          }
+        },
       });
-    }, [near]);
-  
-    const requestSignIn = useCallback(
-      (e) => {
-        e && e.preventDefault();
-        walletModal.show();
-        return false;
-      },
-      [walletModal]
-    );
-  
-    const logOut = useCallback(async () => {
-      if (!near) {
-        return;
-      }
-      const wallet = await (await near.selector).wallet();
-      wallet.signOut();
-      near.accountId = null;
-      setSignedIn(false);
-      setSignedAccountId(null);
-    }, [near]);
-  
-    const refreshAllowance = useCallback(async () => {
-      alert(
-        "You're out of access key allowance. Need sign in again to refresh it"
+  }, [initNear]);
+
+  useEffect(() => {
+    if (!near) {
+      return;
+    }
+    near.selector.then((selector) => {
+      setWalletModal(
+        setupModal(selector, { contractId: near.config.contractName })
       );
-      await logOut();
-      requestSignIn();
-    }, [logOut, requestSignIn]);
-    refreshAllowanceObj.refreshAllowance = refreshAllowance;
-  
-    useEffect(() => {
-      if (!near) {
-        return;
-      }
-      setSignedIn(!!accountId);
-      setSignedAccountId(accountId);
-      setConnected(true);
-    }, [near, accountId]);
-  
-    useEffect(() => {
-      setAvailableStorage(
-        account.storageBalance
-          ? Big(account.storageBalance.available).div(utils.StorageCostPerByte)
-          : Big(0)
-      );
-    }, [account]);
-  
-    const passProps = {
-      refreshAllowance: () => refreshAllowance(),
-      setWidgetSrc,
-      signedAccountId,
-      signedIn,
-      connected,
-      availableStorage,
-      widgetSrc,
-      logOut,
-      requestSignIn,
-      widgets: Widgets,
-      documentationHref,
-    };
-  
-    return (
-      <div className="App">
-        <EthersProviderContext.Provider value={ethersProviderContext}>
-          <Router basename={process.env.PUBLIC_URL}>
-            <Switch>
-              <Route path={"/embed/:widgetSrc*"}>
-                <EmbedPage {...passProps} />
-              </Route>
-              <Route path={"/edit/:widgetSrc*"}>
-                <NavigationWrapper {...passProps} />
-                <EditorPage {...passProps} />
-              </Route>
-              <Route path={"/:widgetSrc*"}>
-                <NavigationWrapper {...passProps} />
-                <ViewPage {...passProps} />
-              </Route>
-            </Switch>
-          </Router>
-        </EthersProviderContext.Provider>
-      </div>
+    });
+  }, [near]);
+
+  const requestSignIn = useCallback(
+    (e) => {
+      e && e.preventDefault();
+      walletModal.show();
+      return false;
+    },
+    [walletModal]
+  );
+
+  const logOut = useCallback(async () => {
+    if (!near) {
+      return;
+    }
+    const wallet = await (await near.selector).wallet();
+    wallet.signOut();
+    near.accountId = null;
+    setSignedIn(false);
+    setSignedAccountId(null);
+  }, [near]);
+
+  const refreshAllowance = useCallback(async () => {
+    alert(
+      "You're out of access key allowance. Need sign in again to refresh it"
     );
-  }
-  
-  export default App;
-  
+    await logOut();
+    requestSignIn();
+  }, [logOut, requestSignIn]);
+  refreshAllowanceObj.refreshAllowance = refreshAllowance;
+
+  useEffect(() => {
+    if (!near) {
+      return;
+    }
+    setSignedIn(!!accountId);
+    setSignedAccountId(accountId);
+    setConnected(true);
+  }, [near, accountId]);
+
+  useEffect(() => {
+    setAvailableStorage(
+      account.storageBalance
+        ? Big(account.storageBalance.available).div(utils.StorageCostPerByte)
+        : Big(0)
+    );
+  }, [account]);
+
+  const passProps = {
+    refreshAllowance: () => refreshAllowance(),
+    setWidgetSrc,
+    signedAccountId,
+    signedIn,
+    connected,
+    availableStorage,
+    widgetSrc,
+    logOut,
+    requestSignIn,
+    widgets: Widgets,
+    documentationHref,
+  };
+
+  return (
+    <div className="App">
+      <EthersProviderContext.Provider value={ethersProviderContext}>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path={"/embed/:widgetSrc*"}>
+              <EmbedPage {...passProps} />
+            </Route>
+            <Route path={"/edit/:widgetSrc*"}>
+              <NavigationWrapper {...passProps} />
+              <EditorPage {...passProps} />
+            </Route>
+            <Route path={"/:widgetSrc*"}>
+              <NavigationWrapper {...passProps} />
+              <ViewPage {...passProps} />
+            </Route>
+          </Switch>
+        </Router>
+      </EthersProviderContext.Provider>
+    </div>
+  );
+}
+
+export default App;
