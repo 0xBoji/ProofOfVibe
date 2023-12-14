851b21ca71e5e56a6651a6f62756a2fb49cccb98
pichtranst123 <126134422+pichtranst123@users.noreply.github.com>
2023-12-14 09:02:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data/widgets.js
@@ -0,0 +1,37 @@
+const TestnetDomains = {
+  "test.near.social": true,
+  "127.0.0.1": true,
+};
+
+export const NetworkId =
+  window.location.hostname in TestnetDomains ? "testnet" : "mainnet";
+const TestnetWidgets = {
+  image: "eugenethedream/widget/Image",
+  default: "eugenethedream/widget/Welcome",
+  viewSource: "eugenethedream/widget/WidgetSource",
+  widgetMetadataEditor: "eugenethedream/widget/WidgetMetadataEditor",
+  widgetMetadata: "eugenethedream/widget/WidgetMetadata",
+  profileImage: "eugenethedream/widget/ProfileImage",
+  profilePage: "eugenethedream/widget/Profile",
+  profileName: "eugenethedream/widget/ProfileName",
+  notificationButton: "eugenethedream/widget/NotificationButton",
+};
+
+const MainnetWidgets = {
+  image: "mob.near/widget/Image",
+  default: "proofofvibes.near/widget/Vibes.DAO.main",
+  viewSource: "mob.near/widget/WidgetSource",
+  widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
+  widgetMetadata: "mob.near/widget/WidgetMetadata",
+  profileImage: "mob.near/widget/ProfileImage",
+  notificationButton: "mob.near/widget/NotificationButton",
+  profilePage: "near/widget/ProfilePage",
+  profileName: "patrick.near/widget/ProfileName",
+  editorComponentSearch: "mob.near/widget/Editor.ComponentSearch",
+  profileInlineBlock: "mob.near/widget/Profile.InlineBlock",
+  // viewHistory: "bozon.near/widget/WidgetHistory",
+  viewHistory: "near/widget/ComponentHistory",
+};
+
+export const Widgets =
+  NetworkId === "testnet" ? TestnetWidgets : MainnetWidgets;
